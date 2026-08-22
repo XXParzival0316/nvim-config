4c9685ce1b51c97c6ddf28e1786ac758dee0388f
XXParzival <xxparzival0316@icloud.com>
2026-08-22 17:14:11 +0800

    add keymaps.lua

---
+++ lua/core/keymaps.lua
@@ -0,0 +1,10 @@
+local keymap = vim.keymap
+
+vim.g.mapleader = " " -- 主键
+
+
+-- 分屏
+keymap.set({"n","i"},"<leader>vs","<Cmd>vsplit<CR>",{ silent = true })
+keymap.set({"n","i"},"<leader>hs","<Cmd>split<CR>",{ silent = true })
+
+
