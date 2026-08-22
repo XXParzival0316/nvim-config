9024f398a12ab2293f604d295f87de73e488d075
XXParzival <xxparzival0316@icloud.com>
2026-08-23 02:32:11 +0800

    add blink.cmp and config for it

--- lua/plugins/autocompletion.lua
+++ lua/plugins/autocompletion.lua
@@ -9,5 +9,40 @@ return{
         opts = {
             ensure_installed = {"lua_ls"}
         },
+        event = "VeryLazy"
+    },
+    {
+         -- 自动补全引擎
+         'saghen/blink.cmp',
+          dependencies = { 'rafamadriz/friendly-snippets' },
+          version = '1.*',
+          opts = {
+            keymap = { preset = 'super-tab' },
+            appearance = {
+                nerd_font_variant = 'mono'
+            },
+
+            completion = { documentation = { auto_show = true } },
+
+            sources = {
+                default = { 'lsp', 'path', 'snippets', 'buffer' },
+            },
+            cmdline = {
+                sources = function()
+                    local cmd_type = vim.fn.getcmdtype()
+                    if cmd_type == "/" then
+                        return { "buffer" }
+                    end
+                    if cmd_type == ":" then
+                        return { "cmdline" }
+                    end
+                end,
+                keymap = {
+                    preset = "super-tab"
+                },
+                completion = { menu = { auto_show = true}}
+            },
+          },
+          event = { "InsertEnter", "CmdlineEnter"}
     },
 }
