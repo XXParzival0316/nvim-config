f5fa3d105a1c7c4a5c07bae6a4d44006ae435a0e
XXParzival <xxparzival0316@icloud.com>
2026-08-22 21:28:07 +0800

    add BufferLine plugin and set shortcut for it

--- lua/plugins/UI.lua
+++ lua/plugins/UI.lua
@@ -5,4 +5,16 @@ return{
             vim.cmd[[colorscheme dracula]]
         end,
     },
+    {
+        "akinsho/bufferline.nvim",
+        dependencies = {"nvim-tree/nvim-web-devicons"},
+        opts = {},
+        keys = {
+            {"<leader>h", ":BufferLineCyclePrev<CR>", silent = true },
+            {"<leader>l", ":BufferLineCycleNext<CR>", silent = true },
+            {"<leader>p", ":BufferLinePick<CR>", silent = true },
+            {"<leader>c", ":BufferLinePickClose<CR>", silent = true },
+        }
+    }
+    
 }
