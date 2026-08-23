e80a13aafdb5028830723344577e5155773507a4
XXParzival <xxparzival0316@icloud.com>
2026-08-23 15:49:57 +0800

    shortcut for nhl

--- lua/core/keymaps.lua
+++ lua/core/keymaps.lua
@@ -3,8 +3,11 @@ local keymap = vim.keymap
 vim.g.mapleader = " " -- 主键
 
 
+
 -- 分屏
 keymap.set({"n","i"},"<leader>vs","<Cmd>vsplit<CR>",{ silent = true })
 keymap.set({"n","i"},"<leader>hs","<Cmd>split<CR>",{ silent = true })
+
+keymap.set("n","<Esc><Esc>",":noh<CR>",{ silent = true})
 
 
